1680edd3b7ed199c7b74a2467fa6e26bfa2f8218
danah <ddaana0220@gmail.com>
2025-10-19 15:39:36 +0300

    Initial Commit

---
+++ planto/plantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantoApp.swift
+//  planto
+//
+//  Created by danah alsadan on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
